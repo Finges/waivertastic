0c75f8331ddbb4eb3eff2cfd6f43e40b66f80a8e
Chris Lee <finges@gmail.com>
2013-07-25 06:24:15 +0000

    Initial Commit, Edited Readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Waivertastic
+
+This rails application allows a user to create a custom waiver. The waivers are
+will be filled out online and emailed to the customer. The customer will then be
+able to print/sign their waivers before coming to the users business. The
+information that the customer fills out will be stored in a database, available
+for the business owner to reference. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waivertastic::Application.config.secret_token = '1b6fcaa075efab1ed37253a455dc8d5d402f37da391a676568a3bc94d617e38c018c908039ff3d4a93cfab6c10295ac3ae5d278e52d42fc6d8a3316ef5262eea'
